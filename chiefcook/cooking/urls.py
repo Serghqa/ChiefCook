b2831060ac08df78cdf24844fbc6c3de545006c0
Sergey Ponomarev <psl.ru@mail.ru>
2026-04-27 17:55:53 +0500

    replaced view functions with classes

--- chiefcook/cooking/urls.py
+++ chiefcook/cooking/urls.py
@@ -5,11 +5,11 @@ from . import views
 app_name = "cooking"
 
 urlpatterns = [
-     path("", views.index, name="index"),
-     path("category/<slug:slug>/", views.category, name="category"),
-     path("recipe/<slug:slug>/", views.recipe, name="recipe"),
-     path("tag/<slug:slug>/", views.tag, name="tag"),
-     path("cuisine/<slug:slug>/", views.cuisine, name="cuisine"),
+     path("", views.IndexView.as_view(), name="index"),
+     path("category/<slug:slug>/", views.CategoryView.as_view(), name="category"),
+     path("recipe/<slug:slug>/", views.RecipeView.as_view(), name="recipe"),
+     path("tag/<slug:slug>/", views.TagView.as_view(), name="tag"),
+     path("cuisine/<slug:slug>/", views.CuisineView.as_view(), name="cuisine"),
      path("addrecipe/", views.add_recipe, name="add_recipe"),
      path("add-ingredient-form/", views.add_ingredient_form, name="add_ingredient_form"),
 ]
--- chiefcook/cooking/views.py
+++ chiefcook/cooking/views.py
@@ -1,75 +1,154 @@
+from typing import Any
+
 from django.http import HttpRequest, Http404
-from django.forms import formset_factory, inlineformset_factory
 from django.shortcuts import redirect, render, get_object_or_404
 from django.db.models import QuerySet
-from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, DetailView
 
 from . models import Recipe, Category, Cuisine, Ingredient, Tag, Profile
 from . forms import RecipeForm, IngredientForm, IngredientFormSet
 
 
-def index(request: HttpRequest):
-    recipes: QuerySet[Recipe] = Recipe.objects.select_related("author__user", "category", "cuisine").all()
-    context = {
-        "recipes": recipes,
-    }
+# def index(request: HttpRequest):
+#     recipes: QuerySet[Recipe] = Recipe.objects.select_related("author__user", "category", "cuisine").all()
+#     context = {
+#         "recipes": recipes,
+#     }
 
-    return render(request, "cooking/index.html", context)
+#     return render(request, "cooking/index.html", context)
 
+class IndexView(ListView):
+    template_name = "cooking/index.html"
+    context_object_name = "recipes"
 
-def cuisine(request: HttpRequest, slug: str):
-    cuisine: Cuisine = get_object_or_404(Cuisine, slug=slug)
-    recipes: QuerySet[Recipe] = Recipe.objects.select_related(
-        "author__user", "category", "cuisine"
-    ).filter(cuisine=cuisine)
-    context = {
-        "recipes": recipes,
-        "cuisine": cuisine,
-    }
-
-    return render(request, "cooking/cuisine.html", context)
+    def get_queryset(self):
+        return Recipe.objects.select_related("author__user", "category", "cuisine").all()
 
 
-def category(request: HttpRequest, slug: str):
-    category: Category = get_object_or_404(Category, slug=slug)
-    recipes: QuerySet[Recipe] = Recipe.objects.select_related(
-        "author__user", "category", "cuisine"
-    ).filter(category=category)
-    context = {
-        "category": category,
-        "recipes": recipes,
-    }
+# def cuisine(request: HttpRequest, slug: str):
+#     cuisine: Cuisine = get_object_or_404(Cuisine, slug=slug)
+#     recipes: QuerySet[Recipe] = Recipe.objects.select_related(
+#         "author__user", "category", "cuisine"
+#     ).filter(cuisine=cuisine)
+#     context = {
+#         "recipes": recipes,
+#         "cuisine": cuisine,
+#     }
 
-    return render(request, "cooking/category.html", context)
+#     return render(request, "cooking/cuisine.html", context)
+
+class CuisineView(ListView):
+    template_name = "cooking/cuisine.html"
+    context_object_name = "recipes"
+
+    def get_queryset(self):
+        slug = self.kwargs["slug"]
+        self.cuisine = get_object_or_404(Cuisine, slug=slug)
+        return Recipe.objects.select_related(
+            "author__user", "category", "cuisine"
+        ).filter(cuisine=self.cuisine)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["cuisine"] = self.cuisine
+        return context
 
 
-def recipe(request: HttpRequest, slug: str):
-    recipe: Recipe = get_object_or_404(
-        Recipe.objects.select_related("author__user", "category", "cuisine").prefetch_related("ingredients"),
-        slug=slug
-    )
-    category: Category = recipe.category
-    ingredients: QuerySet[Ingredient] = recipe.ingredients.all()
-    context = {
-        "recipe": recipe,
-        "category": category,
-        "ingredients": ingredients,
-    }
+# def category(request: HttpRequest, slug: str):
+#     category: Category = get_object_or_404(Category, slug=slug)
+#     recipes: QuerySet[Recipe] = Recipe.objects.select_related(
+#         "author__user", "category", "cuisine"
+#     ).filter(category=category)
+#     context = {
+#         "category": category,
+#         "recipes": recipes,
+#     }
 
-    return render(request, "cooking/recipe.html", context)
+#     return render(request, "cooking/category.html", context)
 
 
-def tag(request: HttpRequest, slug: str):
-    tag: Tag = get_object_or_404(Tag, slug=slug)
-    recipes: QuerySet[Recipe] = Recipe.objects.select_related(
-        "author__user", "category", "cuisine"
-    ).filter(tags=tag)
-    context = {
-        "tag": tag,
-        "recipes": recipes,
-    }
+class CategoryView(ListView):
+    template_name = "cooking/category.html"
+    context_object_name = "recipes"
 
-    return render(request, "cooking/tag.html", context)
+    def get_queryset(self):
+        slug = self.kwargs["slug"]
+        self.category = get_object_or_404(Category, slug=slug)
+        return Recipe.objects.select_related(
+            "author__user", "category", "cuisine"
+        ).filter(category=self.category)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["category"] = self.category
+        return context
+
+
+# def recipe(request: HttpRequest, slug: str):
+#     recipe: Recipe = get_object_or_404(
+#         Recipe.objects.select_related("author__user", "category", "cuisine").prefetch_related("ingredients"),
+#         slug=slug
+#     )
+#     category: Category = recipe.category
+#     ingredients: QuerySet[Ingredient] = recipe.ingredients.all()
+#     context = {
+#         "recipe": recipe,
+#         "category": category,
+#         "ingredients": ingredients,
+#     }
+
+#     return render(request, "cooking/recipe.html", context)
+
+
+class RecipeView(DetailView):
+    template_name = "cooking/recipe.html"
+    context_object_name = "recipe"
+
+    def get_object(self, queryset=None):
+        slug = self.kwargs["slug"]
+        recipe = get_object_or_404(
+            Recipe.objects.select_related("author__user", "category", "cuisine").prefetch_related("ingredients"),
+            slug=slug
+        )
+        self.category = recipe.category
+        self.ingredients = recipe.ingredients.all()
+        return recipe
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["category"] = self.category
+        context["ingredients"] = self.ingredients
+        return context
+
+
+# def tag(request: HttpRequest, slug: str):
+#     tag: Tag = get_object_or_404(Tag, slug=slug)
+#     recipes: QuerySet[Recipe] = Recipe.objects.select_related(
+#         "author__user", "category", "cuisine"
+#     ).filter(tags=tag)
+#     context = {
+#         "tag": tag,
+#         "recipes": recipes,
+#     }
+
+#     return render(request, "cooking/tag.html", context)
+
+
+class TagView(ListView):
+    template_name = "cooking/tag.html"
+    context_object_name = "recipes"
+
+    def get_queryset(self):
+        slug = self.kwargs["slug"]
+        self.tag = get_object_or_404(Tag, slug=slug)
+        return Recipe.objects.select_related(
+            "author__user", "category", "cuisine"
+        ).filter(tags=self.tag)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tag"] = self.tag
+        return context
 
 
 def add_recipe(request: HttpRequest):
