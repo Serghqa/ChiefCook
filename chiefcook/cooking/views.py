ada7ec44bf7fd3c91a0a75f221e21248f7a3a4de
Sergey Ponomarev <psl.ru@mail.ru>
2026-05-06 16:16:34 +0500

    add include pagination for templates

--- chiefcook/cooking/views.py
+++ chiefcook/cooking/views.py
@@ -1,10 +1,9 @@
-from django.http import HttpRequest, Http404
-from django.urls import reverse
+from django.http import HttpRequest
 from django.shortcuts import redirect, render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.decorators import login_required
 
-from . models import Profile, Recipe, Category, Cuisine, Tag
+from . models import Recipe, Category, Cuisine, Tag
 from . forms import RecipeForm, IngredientFormSet
 from . utils import RecipeQuerysetMixin, FilteredQuerysetMixin
 
