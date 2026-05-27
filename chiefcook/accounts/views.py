826bcd1be48b9140349b2c7fbf98a8cc5fd7674a
Sergey Ponomarev <psl.ru@mail.ru>
2026-05-27 13:09:12 +0500

    add link for admin

--- chiefcook/accounts/views.py
+++ chiefcook/accounts/views.py
@@ -8,9 +8,7 @@ from django.contrib.auth.views import (
     PasswordResetCompleteView,
 )
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
-from django.contrib.auth import get_user_model
 
 from .forms import (
     UserRegisterForm,
@@ -59,22 +57,20 @@ class UserPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
     success_url = reverse_lazy("accounts:password_change_done")
 
 
-class UserPasswordResetView(SuccessMessageMixin, PasswordResetView):
+class UserPasswordResetView(PasswordResetView):
     form_class = UserPasswordResetForm
     template_name = "accounts/reset_password.html"
     email_template_name = "accounts/reset_content_email.html"
     success_url = reverse_lazy("accounts:password_reset_done")
-    success_message = "Инструкции по востановлению пароля отправлены на вашу почту"
 
 
 class UserPasswordResetDoneView(PasswordResetDoneView):
     template_name = "accounts/password_reset_done.html"
 
 
-class UserPasswordResetConfirmView(SuccessMessageMixin, PasswordResetConfirmView):
+class UserPasswordResetConfirmView(PasswordResetConfirmView):
     template_name = "accounts/password_reset_confirm.html"
     success_url = reverse_lazy("accounts:password_reset_complete")
-    success_message = "Пароль успешно сохранен"
 
 
 class UserPasswordResetCompleteView(PasswordResetCompleteView):
